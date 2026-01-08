5a823deb4497dcbc76970a198f33428de0979e07
Matthew Chua <matthew@chua.io>
2026-01-08 12:53:23 +1030

    chore(devops): update powersync url env var

--- apps/app-bible/app.config.ts
+++ apps/app-bible/app.config.ts
@@ -219,8 +219,14 @@ const base = {
 
 module.exports = () => {
   const environment = process.env['EXPO_PUBLIC_ENVIRONMENT'] || 'development';
-  const resolveEnv = (key: string, fallbackKey?: string) =>
-    process.env[key] ?? (fallbackKey ? process.env[fallbackKey] : undefined);
+  const resolveEnv = (...keys: string[]) => {
+    for (const key of keys) {
+      if (process.env[key]) {
+        return process.env[key];
+      }
+    }
+    return undefined;
+  };
 
   const config = {
     ...base,
@@ -245,8 +251,8 @@ module.exports = () => {
         'SUPABASE_ANON_KEY'
       ),
       EXPO_PUBLIC_POWERSYNC_URL: resolveEnv(
-        'EXPO_PUBLIC_POWERSYNC_URL',
-        'POWERSYNC_URL'
+        'EXPO_PUBLIC_POWERSYNC_BIBLE_URL',
+        'POWERSYNC_BIBLE_URL'
       ),
     },
   };
